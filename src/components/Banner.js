cf8197c7c88130b585b23f4b699b3112ba073ee3
hieun614 <hieun614@gmail.com>
2023-08-22 16:34:25 +0700

    first-commit-22/08/2023

---
+++ src/components/Banner.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "../assets/img/house-banner.png";
+import Search from "../components/Search";
+
+const Banner = () => {
+  return (
+    <section
+      className="h-full max-h-[640px] mb-8
+    xl:mb-24"
+    >
+      <div className="flex flex-col lg:flex-row">
+        <div
+          className="lg:ml-8 xl:ml-[135px] flex
+        flex-col items-center lg:items-start 
+        lb:text-left justify-center flex-1
+        px-4 lg:px-0"
+        >
+          <h1
+            className="text-4xl lg:text-[58px]
+          font-semibold leading-none mb-6"
+          >
+            <span className="text-violet-700">Rent</span> You Dream House with
+            us
+          </h1>
+          <p className="max-w-[480px] mb-8">
+            Trong tương lai, tôi đang lên kế hoạch để làm cho các lớp có thể
+            được nhấp chuột, vì vậy khi người dùng nhấp vào một lớp, nó sẽ được
+            chèn vào phần HTML.
+          </p>
+        </div>
+        <div className="hidden flex-1 lg:flex mr-32">
+          <img src={Image}></img>
+        </div>
+      </div>
+      <Search />
+    </section>
+  );
+};
+
+export default Banner;
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      className="bg-gray-200  text-center
+    text-white"
+    >
+      <div className="container mx-auto text-black py-6">
+        <h2 className="py-2">@ Copyright 2023 All rights reserved.</h2>
+        <h2>Contact – Privacy policy – Terms of Use</h2>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
